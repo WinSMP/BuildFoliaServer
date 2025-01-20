bf9cbcfc24e763e4a19d71d65eb384096beddcb1
winlogon <walker84837@gmail.com>
2025-01-20 22:47:53 +0100

    refactor(build): improve build script organization and dependencies

--- build_folia.sh
+++ build_folia.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env /bin/bash
 
-pacman -Sy jdk21-openjdk
 TEMP_FILE=$(mktemp -d)
-git clone https://github.com/PaperMC/Folia.git $TEMP_FILE
-cd $TEMP_FILE
-git config --global user.name '#84837'
-git config --global user.email 'walker84837@gmail.com'
+build_pkgs = ('jdk21-openjdk' 'git')
+
+install_deps() {
+	for pkg in ${build_pkgs[@]}; do
+		pacman -Sy --noconfirm $pkg
+	done
+}
+
+set_git_creds() {
+	git config --global user.name 'winlogon'
+	git config --global user.email 'winlogon@winlogon.org'
+}
+
+clone_cd_repo() {
+	git clone https://github.com/PaperMC/Folia.git $TEMP_FILE
+	cd $TEMP_FILE
+	set_git_creds
+}
+
+install_deps
+clone_cd_repo
+set_git_creds
+
 ./gradlew applyPatches
-./gradlew createReobfBundlerJar
+./gradlew createMojmapBundlerJar
